4d524c9df823263fa6a2f0a526f3465b41c4b829
Elise Combette <elise.combette@gmail.com>
2023-07-08 17:11:12 +0200

    Clean up node code

--- Assets/Scripts/AI/Node.cs
+++ Assets/Scripts/AI/Node.cs
@@ -12,9 +12,6 @@ public class Node : MonoBehaviour
     [SerializeField]
     private List<Node> _neighbours;
 
-    //[SerializeField]
-    //private MeshRenderer _meshRenderer;
-
     [SerializeField] private float _searchRadius = 1.1f;
 
     [SerializeField]
@@ -43,19 +40,15 @@ public class Node : MonoBehaviour
         else
             _neighbours.Clear();
 
-        //var bounds = _meshRenderer.bounds;
-        var boundsCenter = transform.position;
-        //var boundsSize = _radius;
-        //float searchRadius = Mathf.Max(boundsSize.x, boundsSize.z) + .1f;
+        var nodePosition = transform.position;
         float sqrSearchRadius = _searchRadius * _searchRadius;
-
         var gridNodes = _gridReference.Nodes;
         foreach(var node in gridNodes)
         {
             if(node == this)
                 continue;
 
-            if(Vector3.SqrMagnitude(boundsCenter - node.transform.position) <= sqrSearchRadius)
+            if(Vector3.SqrMagnitude(nodePosition - node.transform.position) <= sqrSearchRadius)
                 _neighbours.Add(node);
         }
 

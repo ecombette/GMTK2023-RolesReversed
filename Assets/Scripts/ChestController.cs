63ba0c90acb2ccc8384f887e06b146821debfbb1
Théodore <t.pelluard@yahoo.fr>
2023-07-09 12:34:17 +0200

    [FX] Chest dead fx

--- Assets/Scripts/ChestController.cs
+++ Assets/Scripts/ChestController.cs
@@ -26,6 +26,8 @@ public class ChestController : MonoBehaviour
     [Header("Animation")]
     [SerializeField] Animator _animator;
     private readonly int HASH_UP = Animator.StringToHash("XAngle");
+    [SerializeField] Transform _chestDead;
+    [SerializeField] GameObject _groundCollider;
 
     private float _movementTimer = 0f;
 
@@ -106,7 +108,9 @@ public class ChestController : MonoBehaviour
                 t += Time.deltaTime * _movementSpeed;
 
                 transform.position = Vector3.Lerp(startPos, new Vector3(targetPos.x, _movementYCurve.Evaluate(t), targetPos.z), _movementCurve.Evaluate(t));
-                _chest.rotation = Quaternion.Slerp(startRot, targetRot, _rotationCurve.Evaluate(t));
+                Quaternion newRot = Quaternion.Slerp(startRot, targetRot, _rotationCurve.Evaluate(t));
+                _chest.rotation = newRot;
+                _chestDead.rotation = newRot;
 
                 if (_animator)
                     _animator.SetFloat(HASH_UP, Vector3.Dot(transform.up, _chest.up));
@@ -143,6 +147,16 @@ public class ChestController : MonoBehaviour
             transform.localScale = new Vector3(_scaleCurveX.Evaluate(t), _scaleCurveY.Evaluate(t), _scaleCurveZ.Evaluate(t));
             yield return null;
         }
+    }
+
+    [ContextMenu("Kill Chest")]
+    public void KillChest()
+    {
+        _groundCollider.SetActive(true);
+        _chest.gameObject.SetActive(false);
+        _chestDead.gameObject.SetActive(true);
+        playSound(_dieSFX);
+        this.enabled = false;
     }
 
     private void OnTriggerEnter(Collider other)
